20ede511a350b60cc5ade58151af9bc9d129bd57
tannguyen.bk10 <tannguyen.bk10@gmail.com>
2016-03-06 00:11:33 +0700

    high light the active item

--- vmplay.science4you.org_files/vmplay.science4you.org/platform/mo/nabeat/platform/common/scripts/searchmenu.js
+++ vmplay.science4you.org_files/vmplay.science4you.org/platform/mo/nabeat/platform/common/scripts/searchmenu.js
@@ -6,6 +6,9 @@ SearchMenu = function () {
 
 		var id = 1;
 
+		var curPtnameId = getUrlParameter(window.location.search.substring(1), 'ptnameid');
+		var isActive = false;
+
 		$('ul.taxalist > li.taxalist').each(function(i) {
 
 			var $this = $(this);
@@ -13,6 +16,17 @@ SearchMenu = function () {
 			var text = $this.find('a > p:first').text();
 			var description = $this.find('.sciencename').text();
 			var url = $this.find('a').attr('href');
+
+			if (!isActive) {
+
+				var ptnameId = getUrlParameter(url, 'ptnameid');
+
+				if (ptnameId == curPtnameId) {
+
+					isActive = true;
+					$this.css({'background-color': '#99cc00'})
+				}				
+			}
 
 			data.push({
 				id: id,
@@ -24,6 +38,24 @@ SearchMenu = function () {
 			id++;
 
 		});
+	}
+
+	var getUrlParameter = function getUrlParameter(url, sParam) {
+
+		// var sPageURL = decodeURIComponent(window.location.search.substring(1))
+
+	    var sPageURL = decodeURIComponent(url),
+	        sURLVariables = sPageURL.split('&'),
+	        sParameterName,
+	        i;
+
+	    for (i = 0; i < sURLVariables.length; i++) {
+	        sParameterName = sURLVariables[i].split('=');
+
+	        if (sParameterName[0].toLowerCase() === sParam.toLowerCase()) {
+	            return sParameterName[1] === undefined ? true : sParameterName[1];
+	        }
+	    }
 	}
 
 	return {
@@ -39,7 +71,12 @@ SearchMenu = function () {
 			}).on('change', function (e) {
 
 				var baseUrl = 'http://vmplay.science4you.org/';
-				window.location.href = baseUrl + $(this).select2('data')[0].url;
+				var navigateUrl = $(this).select2('data')[0].url;
+				
+				if (typeof navigateUrl != 'undefined') {
+					window.location.href = baseUrl + navigateUrl;
+				}
+				
         	});
 		}
 	}
